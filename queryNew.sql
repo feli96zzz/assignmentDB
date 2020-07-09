80d422eb2a1cfde1f7ce5f9c1e9d2e98ab6d9024
MinhTri1401 <64025293+MinhTri1401@users.noreply.github.com>
2020-07-09 22:01:00 +0700

    Add trigger and function

--- queryNew.sql
+++ queryNew.sql
@@ -484,6 +484,22 @@ GO
 
 
 ------------------------------------^^^^^^ trigger tính thuộc tính dẫn xuất giá đơn hàng trong đơn hàng
+Create trigger checkStatusProductModel
+on Co_Don_hang_nha_ban_mau_sp
+After Insert
+as
+Begin
+	Declare @Conhang int
+	Declare @id int
+	Select @id= id_productModel from inserted
+	Select @Conhang = count(*) from product where product.id_product=@id and product.stock_out_date is NULL
+	if(@Conhang=0) 
+	Begin
+		Update dbo.productModel
+		Set productModel.status='Not available'
+		Where productModel.id=@id
+	End
+End
 
 ---------vvvvvvvvvvvvvv function tính doanh thu trong một khoảng thời gian
 
@@ -550,7 +566,14 @@ GO
 
 ---------^^^^^^^^^^^^^^ function tính doanh thu trong một khoảng thời gian
 
-
+------------------Bang so luong hang da ban theo id---------------------------------------------------------------
+Create function SoLuongSP(@fromDate Date,@toDate Date)
+Returns table as
+Return
+	Select  product.id_product, count(*)as SL, product.stock_out_date
+	From	product
+	Where	@fromDate<=product.stock_out_date and product.stock_out_date <= @toDate and stock_out_date is not NULL 
+	Group by product.id_product, stock_out_date
 -----------------------vvvvvvvvvvvvvvvvvvv function tính giá hóa đơn
 create FUNCTION calPrice (@id_receipt int)
 Returns int as 
